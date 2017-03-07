11975090ef0dad62e725e47c39309f008ca47cc1
Crystal0108W <crystal0108wong@gmail.com>
2017-03-07 09:10:29 -0600

    Update GGplot2.R

--- GGplot2.R
+++ GGplot2.R
@@ -10,22 +10,22 @@ library(gridExtra)
 
 ggplot(data = mtcars, aes(x=wt, y=mpg)) + geom_point() + labs(title = "Automobile Data", x = "Weight", y = "Miles Per Gallon")
 ggplot(data = mtcars, aes(x=wt, y=mpg)) + geom_point(pch = 17, color = "pink", size=2) + 
-                                          geom_smooth(method = "lm", color = "salmon", linetype = 2) + 
-                                          labs(title = "Automobile Data", x = "Weight", y = "Miles Per Gallon")
+  geom_smooth(method = "lm", color = "salmon", linetype = 2) + 
+  labs(title = "Automobile Data", x = "Weight", y = "Miles Per Gallon")
 
 # Grouping and Faceting
 mtcars$am <- factor(mtcars$am, levels = c(0,1),
-                                labels = c("Automatic", "Manual"))
+                    labels = c("Automatic", "Manual"))
 mtcars$vs <- factor(mtcars$vs, levels = c(0,1), 
-                                labels = c("V-Engine", "Straight Engine"))
+                    labels = c("V-Engine", "Straight Engine"))
 mtcars$cyl <- factor(mtcars$cyl)
 
 ggplot(data = mtcars, aes(x = hp, y = mpg,
                           shape = cyl, color = cyl)) +
-                          geom_point(size = 3) + 
-                          facet_grid(am~vs) + 
-                          labs(title="Automobile Data by Engine Type",
-                               x="Horsepower", y="Miles Per Gallon")
+  geom_point(size = 3) + 
+  facet_grid(am~vs) + 
+  labs(title="Automobile Data by Engine Type",
+       x="Horsepower", y="Miles Per Gallon")
 
 data("singer", package = "lattice")
 ggplot(singer, aes(x=height)) + geom_histogram()
@@ -36,25 +36,39 @@ install.packages("car")
 library(car)
 
 ggplot(Salaries, aes(x=rank, y=salary)) + 
-        geom_boxplot(fill = "cornflowerblue",
-                     color = "black", notch = TRUE) + 
-        geom_point(position = "jitter", color = "blue", alpha = .5) + 
-        geom_rug(side = 1, color = "grey")
+  geom_boxplot(fill = "cornflowerblue",
+               color = "black", notch = TRUE) + 
+  geom_point(position = "jitter", color = "blue", alpha = .5) + 
+  geom_rug(side = 1, color = "grey")
 
 ggplot(singer, aes(x = voice.part, y = height)) + 
-        geom_violin(fill = "lightblue") + 
-        geom_boxplot(fill = "pink", width = .2)
+  geom_violin(fill = "lightblue") + 
+  geom_boxplot(fill = "pink", width = .2)
 
 
 # Grouping
 ggplot(data=Salaries, aes(x=salary, fill = rank)) + 
-        geom_density(alpha = .2)
+  geom_density(alpha = .2)
 
 ggplot(Salaries, aes(x=yrs.since.phd, y=salary, color=rank, shape = sex)) + geom_point()
+
 
 ggplot(Salaries, aes(x=rank, fill=sex)) + geom_bar(position = "stack") + labs(title="position = 'stack'")
 ggplot(Salaries, aes(x=rank, fill=sex)) + geom_bar(position = "dodge") + labs(title="position = 'dodge'")
 ggplot(Salaries, aes(x=rank, fill=sex)) + geom_bar(position = "fill") + labs(title="position = 'fill'", y = "Proportion")
+
+
+# Faceting (trellis graph)
+ggplot(data = singer, aes(x = height)) + 
+        geom_histogram() + 
+        facet_wrap(~voice.part, nrow = 4)
+
+
+ggplot(Salaries, aes(x=yrs.since.phd, y=salary, color = rank, shape = rank)) + geom_point() + facet_grid(.~sex)  
+ggplot(Salaries, aes(x=yrs.since.phd, y=salary, color = rank, shape = rank)) + geom_point() + facet_grid(sex~.)  
+ggplot(data = singer, aes(x=height, fill=voice.part))+geom_density() + facet_grid(voice.part~.)
+
+
 
 
 
