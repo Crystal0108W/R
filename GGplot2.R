0403d4a4a0f042c26eea6beb3e8f3c2fc4d467d2
Crystal0108W <crystal0108wong@gmail.com>
2017-03-07 17:00:07 -0600

    Update GGplot2.R

--- GGplot2.R
+++ GGplot2.R
@@ -154,7 +154,7 @@ mytheme <- theme(plot.title = element_text(face = "bold.italic",
                  panel.grid.minor.x = element_blank(),
                  legend.position = "top")
 
-<- ggplot(Salaries, aes(x=rank, y=salary, fill=sex)) + 
+ggplot(Salaries, aes(x=rank, y=salary, fill=sex)) + 
          geom_boxplot() + 
          labs(title = "Salary by Rank and Sex", x="Rank", y="Salary") + 
          mytheme
