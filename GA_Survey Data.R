4f6e4d5b0ae2cb5d934c3996c102daf2d1c66b1e
Crystal0108W <crystal0108wong@gmail.com>
2017-03-20 13:29:58 -0500

    Create GA_Survey Data.R

---
+++ GA_Survey Data.R
@@ -0,0 +1,12 @@
+SurveyList <- unique(PP$Source)  # Check for unique values for Survey Sources
+SurveyList <- as.list(SurveyList)
+
+# extract and arrange survey data for each survey in the survey list
+for (i in 1:6) {
+  Name <- subset(PP, Source == SurveyList[i])
+  Name <- Name[, c(9, 8, 1, 6)]
+  filename <- paste('/Users/yw7986/Desktop/', SurveyList[i], '.csv', sep = "")
+  write.csv(Dating, file = filename)
+}
+# Use for loop to repeat steps that operate on the same codes
+# Use paste to help write and save csv files with different names within for loop
