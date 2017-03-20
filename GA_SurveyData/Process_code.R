97724442cc0b94e2bbd3637800f7d136e9d19742
Crystal0108W <crystal0108wong@gmail.com>
2017-03-20 13:39:04 -0500

    Update Process_code.R

--- GA_SurveyData/Process_code.R
+++ GA_SurveyData/Process_code.R
@@ -6,7 +6,7 @@ for (i in 1:6) {
   Name <- subset(PP, Source == SurveyList[i])
   Name <- Name[, c(9, 8, 1, 6)]
   filename <- paste('/Users/yw7986/Desktop/', SurveyList[i], '.csv', sep = "")
-  write.csv(Dating, file = filename)
+  write.csv(Name, file = filename)
 }
 # Use for loop to repeat steps that operate on the same codes
 # Use paste to help write and save csv files with different names within for loop
