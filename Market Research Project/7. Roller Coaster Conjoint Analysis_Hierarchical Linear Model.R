99c7a0c64cb97301fcef8526e353b4805cc1cc16
Crystal0108W <crystal0108wong@gmail.com>
2017-03-24 14:45:55 -0500

    Update 7. Roller Coaster Conjoint Analysis_Hierarchical Linear Model.R

--- Market Research Project/7. Roller Coaster Conjoint Analysis_Hierarchical Linear Model.R
+++ Market Research Project/7. Roller Coaster Conjoint Analysis_Hierarchical Linear Model.R
@@ -1,5 +1,7 @@
 # Hierarchical Linear Model - Determine Individual Level Effect - Estimate the values in the model for each respondent
 # Roller Coaster Conjoint Analysis: levels of maximum speed, height, construction type , and theme.
+# when you have multiple observations for an individual or other grouping factor of interest, you should consider a hierarchical model that estimates both sample-level and individual- or group-level effects.
+
 
 # Simulating Ratings-Based Conjoint Data
 set.seed(12345)
@@ -31,8 +33,22 @@ for(i in seq_along(resp.id)) {
 # Regular Linear Modelling 
 summary(conjoint.df)
 by(conjoint.df$rating, conjoint.df$height, mean)
-ride.lm <- lm(rating ~ speed + height + const + theme, data = conjoint.df)
+ride.lm <- lm(rating ~ speed + height + const + theme, data = conjoint.df) # Ficed effects that are estimated at the sample level. 
 summary(ride.lm)
 # The highest rated roller coaster on average would have a top speed of 70 mph, a height of 300 ft, steel construction, and the dragon theme
 # BUT, The coefficients are estimated on the basis of designs that mostly combine both desirable and undesirable attributes, and are not as reliable at the extremes of preference. 
 # Additionally, it could happen that few people prefer that exact combination even though the individual features are each best on average.
+
+# [Intercept]Hierarchical Linear Model - estimate both the overall average preference level and individual preferences within the group.
+install.packages("lme4")
+install.packages("Matrix")
+ride.hlm1 <- lmer(rating ~ speed + height + const + theme + (1 | resp.id), data = conjoint.df) # For the intercept, that is signified as simply “1”; the grouping variable, for which a random effect will be estimated for each unique group
+# syntax: predictors | group, specify the random effect and grouping variable with syntax using a vertical bar "|"
+# In present case, it is interesting to know the randome effetc("1") of intercept on each individaul level. 
+summary(ride.hlm1)
+fixef(ride.hlm1) # Extract fixed effects at the population level. 
+ranef(ride.hlm1)$resp.id # Extract random effect estimates for intercept 
+coef(ride.hlm1)$resp.id # The complete effect for each respondent comprises the overall fixed effects that apply to everyone + the individually varying random effects 
+
+# [Complete]Hierarchical Linear Model - estimate a random effect parameter for every coefficient of interest for every respondent.
+ride.hlm2 <- lmer(rating ~ speed + height + const + theme + (speed + height + const + theme | resp.id), data = conjoint.df, control = lmerControl(optCtrl = list(maxfun = 100000)) # control argument increases the maxfun number of iterations to attempt convergence from 10,000 iterations (the default) to 100,000. This allows the model to converge better
