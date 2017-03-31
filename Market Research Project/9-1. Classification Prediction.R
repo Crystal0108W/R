38cf7c069aea2efbc7c36944ed4dcf213eb124f2
Crystal0108W <crystal0108wong@gmail.com>
2017-03-30 22:47:27 -0500

    Create 9-1. Classification Prediction.R

---
+++ Market Research Project/9-1. Classification Prediction.R
@@ -0,0 +1,14 @@
+# Prediction: Classification 
+# Problem 1: predicting segment membership
+# Problem 2: predicting who is likely to subscribe to a service
+
+# Steps: 
+# 1, A data set is collected in which group membership for each observation is known or assigned
+# (e.g., assigned by behavioral observation, expert rating, or clustering procedures).
+# 2. The data set is split into a training set and a test set. A common pattern is to select 50–80 % of the observations for the training set (67 % seems to be particularly common), and to assign the remaining observations to the test set. 
+# 3. A prediction model is built, with a goal to predict membership in the training data as well as possible.
+# 4. The resulting model is then assessed for performance using the test data. Per- formance is assessed to see that it exceeds chance (base rate). Additionally one might assess whether the method performs better than a reasonable alternative (and simpler or better-known) model.
+
+# naive Bayes classifiers
+
+# random forest classifiers
